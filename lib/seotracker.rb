79254b1ed9c44e9819dbcb8af5c4d5585338dd81
krivich ekaterina <kiote_the_one@mail.ru>
2012-04-04 15:22:19 +0400

    get wordstat

--- lib/seotracker.rb
+++ lib/seotracker.rb
@@ -11,6 +11,13 @@ class Seotracker
     @agent.user_agent_alias = Seotracker::USER_AGENT
     @agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     @agent.agent.http.retry_change_requests = true
+
+    @debug = true
+
+    if @debug
+      @log = Logger.new('log.txt')
+      #@agent.log = @log если надо логировать mechnize
+    end
   end
 
   def get_position(site, word)
@@ -18,6 +25,9 @@ class Seotracker
     while (start < 200) && !found
       links = parse(word, start)
       start += RESULTS
+
+      break if links == 'error'
+
       links.each do |l|
         pos += 1
         href = l.attribute('href').value.downcase
@@ -28,6 +38,10 @@ class Seotracker
       end
     end
     found ? pos : 0
+  end
+
+  def debug(message)
+    @log.debug(message) if @debug
   end
 end
 
--- lib/seotracker/yandex.rb
+++ lib/seotracker/yandex.rb
@@ -1,24 +1,39 @@
 class Seotracker::Yandex < Seotracker
-  SEARCH_URL = "http://yandex.ru/yandsearch?"
+  SEARCH_URL = 'http://yandex.ru/yandsearch?'
+  WORDSTAT_URL = 'http://wordstat.yandex.ru/?cmd=words&page=1&&geo=&text_geo=&text='
+
+  def get_wordstat(word)
+    url = WORDSTAT_URL + word
+    get_cookie if @cookie.nil?
+    page = @agent.get(url, [], nil, {'cookie' => @cookie})
+    res = page.root.xpath('/html/body/form/table[2]/tbody/tr/td[4]/table/tbody/tr[3]/td/table/tbody/tr[2]/td[3]')
+    begin
+      res.first.content
+    rescue Exception => e
+      debug "can't get yandex wordstat:" + e.message
+      0
+    end
+  end
 
   protected
 
   def parse(word, start = 0)
     start /= 10
-
-    # у яндекса хитрая проверка на роботов
-    if start == 0
-      @agent.get('http://kiks.yandex.ru/su/')
-      @cookie = @agent.cookies.first
-    end
-
-    #agent = Mechanize.new
-    #agent.user_agent_alias = Seotracker::USER_AGENT
-    ##agent.log = Logger.new('log.txt')
-
+    get_cookie if @cookie.nil?
     url = SEARCH_URL + "text=#{word}&p=#{start}"
-
     page = @agent.get(url, [], nil, {'cookie' => @cookie})
-    page.root.xpath('/html/body/div[3]/div/div/div[2]/ol/li/div/h2/a')
+
+    begin
+      page.root.xpath('/html/body/div[3]/div/div/div[2]/ol/li/div/h2/a')
+    rescue Exception => e
+      debug "can't parse yandex:" + e.message
+      'error'
+    end
+  end
+
+  def get_cookie
+    # у яндекса хитрая проверка на роботов
+    @agent.get('http://kiks.yandex.ru/su/')
+    @cookie = @agent.cookies.first
   end
 end
